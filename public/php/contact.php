084471953a070e7c7dbe009e2542da4f692cf36d
Murad Alwan <free1soft@gmail.com>
2021-08-24 16:16:26 +0300

    add tastes pictures  att2

--- public/php/contact.php
+++ public/php/contact.php
@@ -113,8 +113,8 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'info@eatfranchise.com';                     //SMTP username
     $mail->Password   = 'info=2021*-';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('info@eatfranchise.com', 'Mailer');
