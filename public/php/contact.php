48ac4b9c3319e9b8b571d67f3b95c0c72ef40222
Murad Alwan <free1soft@gmail.com>
2021-08-24 16:30:12 +0300

    add tastes pictures  att3

--- public/php/contact.php
+++ public/php/contact.php
@@ -107,7 +107,7 @@ try {
 
 
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.yandex.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
